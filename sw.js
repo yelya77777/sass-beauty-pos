2b398c52c9031028e218a7954735f2434bc623ba
yelya77777 <8hhs472c24@privaterelay.appleid.com>
2026-08-10 14:34:21 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-const CACHE='sass-beauty-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+const CACHE='sass-beauty-v2';
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
